5a391023ac8b8654680ab68983c70ab9d1ea3100
Ian Newborn <ian.newborn@schneider-electric.com>
2015-09-17 17:30:03 +0100

    Initial work

---
+++ mixer.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+)
+
+func test(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	body, _ := ioutil.ReadAll(r.Body)
+	var bodyText = string(body)
+	fmt.Println(bodyText)
+	io.WriteString(w, "Testing testing 1 2 3. "+bodyText)
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.HandleFunc("/test", test)
+	http.ListenAndServe(":8000", mux)
+}
